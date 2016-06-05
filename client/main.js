0e3cc658229f4ccb9c7422409e26fe198c04166f
knipferrc <knipferrc@gmail.com>
2016-06-05 17:43:39 -0400

    Getting set up

--- client/main.js
+++ client/main.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import AppBar from 'material-ui/AppBar';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {purple800} from 'material-ui/styles/colors';
+
+const styles = {
+  appBarStyle: {
+    backgroundColor: purple800
+  }
+};
 
 const App = () => (
-  <h1>Hello World</h1>
+ <MuiThemeProvider  >
+    <AppBar
+      title="Resuview"
+      iconClassNameRight="muidocs-icon-navigation-expand-more"
+      style={styles.appBarStyle}
+     />
+  </MuiThemeProvider>
 );
 
 export default App;
--- client/startup.js
+++ client/startup.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {render} from 'react-dom';
 import {Meteor} from 'meteor/meteor';
 import App from './main.js';
+import 'normalize.css/normalize.css';
 
 Meteor.startup(() => {
   render(<App />, document.getElementById('app'));
